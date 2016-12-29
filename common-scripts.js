f9344bd0cfc6caf3a7733126d51fa13ab74b3955
Fabian <schoberf@gmx.net>
2016-12-30 00:54:08 +0100

    Templating workds & ShowCase // Moved pages to separate folders

---
+++ common-scripts.js
@@ -0,0 +1,49 @@
+/* Nano Templates - https://github.com/trix/nano */
+function nano(template, data) {
+    return template.replace(/\{([\w\.]*)\}/g, function (str, key) {
+        var keys = key.split("."), v = data[keys.shift()];
+        for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+        return (typeof v !== "undefined" && v !== null) ? v : "";
+    });
+}
+/*End Nano Templates */
+
+
+//THX to http://stackoverflow.com/a/6860916
+function guidGenerator(noTrailingUnderscore) {
+    var S4 = function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    var guid = (noTrailingUnderscore ? "" : "_") + (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+    console.log("Created GUID: " + guid);
+    return guid;
+}
+
+function createListFromFile(listSelector, templateSelector, dataFilePath) {
+    return readDataFile(dataFilePath)
+        .then(result => createListFromData(listSelector, templateSelector, result));
+}
+
+function createListFromData(listSelector, templateSelector, data) {
+    var listElement = $(listSelector);
+    var templateElement = $(templateSelector);
+
+    if (templateElement == null || !templateElement.length)
+        return; //No element found
+
+    var templateStr = templateElement[0].outerHTML;
+
+    for (let element of data) {
+        //MDL needs an unique id for each inpute element. Create a random GUID
+        element.templateId = guidGenerator();
+
+        let filledTemplate = nano(templateStr, element);
+        listElement.append(filledTemplate);
+    }
+}
+
+function readDataFile(dataFilePath) {
+    return new Promise(function (resolve, reject) {
+        $.getJSON(dataFilePath).done(resolve).fail(reject);
+    });
+}
---
+++ mobile/overview/overview.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    createListFromFile("#overview-list", ".template > li", "overviewList.json");
+});
